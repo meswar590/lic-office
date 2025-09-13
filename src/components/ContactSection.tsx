d7744cb89c57fcde476cce29687e79129828b750
MADDALI ESWAR <eswar@example.com>
2025-09-13 20:23:19 +0530

    pincode change

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -283,7 +283,7 @@ const ContactSection = () => {
           <Card className="p-2 bg-white shadow-card">
             <div className="aspect-video rounded-lg overflow-hidden">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3829.8421892847565!2d79.9323633!3d15.4080209!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a4b61f7a1f7a1f7%3A0x9b5b5b5b5b5b5b5b!2sLIC%20Branch%20Office%2C%20Main%20Road%2C%20Chimakurthy%2C%20Prakasam%20District%2C%20Andhra%20Pradesh%20523187!5e0!3m2!1sen!2sin!4v1642584760000!5m2!1sen!2sin"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3829.8421892847565!2d79.9323633!3d15.4080209!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a4b61f7a1f7a1f7%3A0x9b5b5b5b5b5b5b5b!2sLIC%20Branch%20Office%2C%20Main%20Road%2C%20Chimakurthy%2C%20Prakasam%20District%2C%20Andhra%20Pradesh%20523226!5e0!3m2!1sen!2sin!4v1642584760000!5m2!1sen!2sin"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
