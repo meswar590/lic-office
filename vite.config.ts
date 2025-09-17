25dd9f4ee7a0461265ca415fc5270b1362c1e2ca
MADDALI ESWAR <eswar@example.com>
2025-09-18 00:45:32 +0530

    Setup GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/lic-office/",   // 👈 this is your repo name
+
   server: {
     host: "::",
     port: 8080,
