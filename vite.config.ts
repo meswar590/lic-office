07b6e11acbaaa89868dda5757bb9b5fc1db2deea
MADDALI ESWAR <eswar@example.com>
2025-09-18 20:33:23 +0530

    vite changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/lic-office/",   // 👈 this is your repo name
-
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/lic-office/" : "/",   // 👈 Only use /lic-office/ in production
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
