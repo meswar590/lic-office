7b6b9017d32e0f1c56624bc61031cecabd293247
MADDALI ESWAR <eswar@example.com>
2025-10-05 09:29:20 +0530

    vite config changed

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/licsreehari/" : "/",   
+  base: command === "build" ? "/lic-office/" : "/",   
   server: {
     host: "::",
     port: 8080,
